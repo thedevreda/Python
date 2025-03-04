70674745d08f8a50b63e4a932f9cc78075d08d9c
thedevreda <redaamelloul@gmail.com>
2025-03-04 15:27:08 +0000

    first commit

---
+++ Before_coding.md
@@ -0,0 +1,9 @@
+- conda create -n [ENV] python=3.11
+- conda env list
+- conda list
+- conda activate [ENV]
+- Install  / Y
+- File > Ctrl + Shift + P > Python : Select Interpreter > [ENV]
+
+- conda deactivate 
+- conda remove -n [ENV] --all
---
+++ global/module.py
@@ -0,0 +1,10 @@
+skills_list = ['Python', 'Tableau', 'Postgres']
+
+# function
+
+def skill(skill_text):
+    """
+    importing the skill text that you put inside of skill() function
+    skill(skill_text: Any)
+    """
+    return f'{skill_text} is my best skill'
